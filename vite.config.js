b418e0d5a11b4f1556b782f36acabcf7fdea5578
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2025-10-31 21:38:56 +0800

    fix(deploy): Set Vite base to '/' to fix asset loading on sub-routes

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
