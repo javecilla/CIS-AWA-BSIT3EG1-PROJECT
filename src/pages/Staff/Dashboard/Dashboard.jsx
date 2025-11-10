b82844abf5128b5d9147c57578ef90af73cde20a
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2025-11-11 00:14:48 +0800

    feat(staff-dashboard): fetch staff metrics dashboard
    - show all registered patients
    - added search, view, edit, delete and pagination functionality for registered patient
    - show all patients appointments
    - added search, pagination, update appointment status for patients appointments

--- src/pages/Staff/Dashboard/Dashboard.jsx
+++ src/pages/Staff/Dashboard/Dashboard.jsx
@@ -1,50 +1,30 @@
 import './Dashboard.css'
 import Header from '@/components/Header'
-import TotalAppointments from '@/components/TotalAppointments/TotalAppointments'
-import PatientRegistered from '@/components/PatientRegistered/PatientRegistered'
-import PatientAppointmentList from '@/components/PatientAppointmentList/PatientAppointmentList'
-import PatientRegisteredList from '@/components/PatientRegisteredList/PatientRegisteredList'
-import { useUser } from '@/contexts/UserContext'
-import { NavLink } from 'react-router-dom'
+import TotalAppointments from '@/components/TotalAppointments'
+import PatientRegistered from '@/components/PatientRegistered'
+import PatientAppointmentList from '@/components/PatientAppointmentList'
+import PatientRegisteredList from '@/components/PatientRegisteredList'
 
 function Dashboard() {
-  const { userData, loading } = useUser()
-  // console.log('User Data in Profile:', userData)
-
-  if (loading) {
-    return (
-      <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    )
-  }
-  
   return (
     <>
       <Header />
 
       <div className="container">
         <div className="row gx-4 gy-4 align-items-stretch profile-consult-row">
-          {/*total appointments*/}
-          <TotalAppointments/>
+          <TotalAppointments />
 
-          {/*registered patients*/}
-          <PatientRegistered/>
+          <PatientRegistered />
         </div>
 
-        {/*Patient Registered List*/}
         <div className="patient-table-container mt-4">
-          <PatientRegisteredList/>
+          <PatientRegisteredList />
         </div>
 
-        {/*Patient Appointment list*/}
         <div className="patient-table-container mt-4">
-          <PatientAppointmentList/>
+          <PatientAppointmentList />
         </div>
       </div>
-      
     </>
   )
 }
