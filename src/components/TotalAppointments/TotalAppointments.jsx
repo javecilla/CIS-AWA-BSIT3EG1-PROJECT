4893208d8c8d5fc03aabff75ed51a679e32f7dc0
Renzen <delacruzrensen@gmail.com>
2025-11-10 19:46:32 +0800

    refactor(ui): Fixed conflicting class names

--- src/components/TotalAppointments/TotalAppointments.jsx
+++ src/components/TotalAppointments/TotalAppointments.jsx
@@ -1,26 +1,20 @@
-import './TotalAppointments.css'
+import './TotalAppointments.css';
 
-function TotalAppointments({
-  totalAppointment,
-  checkedIn,
-  completed,
-  pending
-}) {
+function TotalAppointments({ totalAppointment, checkedIn, completed, pending }) {
+
   return (
     <>
       <div className="total-appointment-container col-12 col-lg-6 d-flex">
-        <i className="fa-solid fa-circle-info"></i>
-        <div className="card-equal consultation-card w-100 d-flex flex-column flex-md-row align-items-center gap-3">
-          <div className="consultation-text d-flex flex-column justify-content-center align-items-center align-items-md-start">
-            <h2 className="consultation-title">Total Appointments</h2>
+        <i class="fa-solid fa-circle-info"></i>
+        <div className="card-equal patient-info-card w-100 d-flex flex-column flex-md-row align-items-center gap-3">
+          <div className="top-card-text d-flex flex-column justify-content-center align-items-center align-items-md-start">
+            <h2 className="top-card-title">
+              Total Appointments
+            </h2>
             <h2>{totalAppointment || 35}</h2>
-            <p className="consultation-description mb-3">
-              Checked-in: {checkedIn}
-            </p>
-            <p className="consultation-description mb-3">
-              Completed: {completed}
-            </p>
-            <p className="consultation-description mb-3">Pending: {pending}</p>
+            <p className="top-card-description mb-3">Checked-in: {checkedIn}</p>
+            <p className="top-card-description mb-3">Completed: {completed}</p>
+            <p className="top-card-description mb-3">Pending: {pending}</p>
           </div>
         </div>
       </div>
@@ -28,4 +22,4 @@ function TotalAppointments({
   )
 }
 
-export default TotalAppointments
+export default TotalAppointments;
